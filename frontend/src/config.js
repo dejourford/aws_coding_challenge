dac0f89deb0163deb4ec03658832dd5ad6dd784b
De'Jour Ford <dejourford@yahoo.com>
2026-06-04 13:39:43 -0500

    update config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://devops-challenge-alb-633282648.us-east-2.elb.amazonaws.com'
+export const API_URL = 'http://devops-challenge-alb-633282648.us-east-2.elb.amazonaws.com/api/'
 export default API_URL
 
 
